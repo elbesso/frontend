5833b1571bd582fd6d3cd9030b93834db4addac2
Dmitry Raguzin <hellomylittleboy@gmail.com>
2015-12-20 20:41:37 +0300

    tbc

--- thankyou.php
+++ thankyou.php
@@ -161,7 +161,6 @@ require("php/setup.php")
             interval: 10000
         })
     </script>
-    <script type="text/javascript" src="<?php echo $localized_recaptcha?>"></script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
